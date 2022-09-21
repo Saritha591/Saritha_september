08c99de62781794f1a285d3d11def8513e6834ae
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-21 16:58:47 +0530

    Add files via upload

---
+++ class self arguments in python idle shell.py
@@ -0,0 +1,10 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: D:/class self arguments.py =====================
+>>> c1= course("python")
+>>> c2= course("html")
+>>> c1.name
+'python'
+>>> c2.name
+'html'
---
+++ class self arguments.py
@@ -0,0 +1,4 @@
+class course:
+    def __init__(self,name):
+        self.name = name
+        self.students =[ ]
