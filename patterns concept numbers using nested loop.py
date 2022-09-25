40987c710ea4891fe0ea838189eba55fb73ec451
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-25 14:11:59 +0530

    Update patterns concept numbers using nested loop.py

--- patterns concept numbers using nested loop.py
+++ patterns concept numbers using nested loop.py
@@ -1,49 +1,49 @@
 #nested loop
 #patterns using this concept write a program to print right angle triangle using numbers
 
-'''i=0
+i=0
 while i<10:
     i+=1
     j=0
     while j<i:
         j+=1
         print(j,end=' ')
-    print( )'''
+    print( )
 # using patterns print reverse of right  angle traingle using nested loop
-'''i=10
+i=10
 while i>=1:
     i-=1
     j=0
     while j<=i:
         j+=1
         print(j,end=' ')
-    print( )'''
+    print( )
 
 # write a program to print right angle traingle in a decrement using nested loop
-'''i=10
+i=10
 while i>=1:
     i-=1
     j=10
     while j>i:
         print(j,end= ' ')
         j=j-1
-    print( )'''
+    print( )
 #write  a program to print right angle traingle with symbols
 #symbol  using *
 
-'''i=0
+i=0
 while i<5:
     i+=1
     j=0
     while j<i:
         j+=1
         print("*",end=' ')
-    print( )'''
+    print( )
 
 
 
 
-'''i=0
+i=0
 n=int(input('enter the n row symbol....'))
 while i<n:
     i+=n
@@ -51,7 +51,7 @@ while i<n:
     while j<i:
         j+=1
         print("*",end=' ')
-    print( )'''
+    print( )
 
 n=int(input('enter the n row symbol....'))
 i=0
