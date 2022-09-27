121e3f89832ca4e31f22a13cdc76bfc14d9e3471
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-27 19:40:04 +0530

    Add files via upload

---
+++ py class dates output 1 idle.py
@@ -0,0 +1,32 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: D:/py class python dates 1.py ====================
+2022-09-27 17:14:51.409987
+>>> 
+==================== RESTART: D:/py class python dates 1.py ====================
+2022-09-27 17:17:14.658806
+2020-05-17 00:00:00
+>>> 
+==================== RESTART: D:/py class python dates 1.py ====================
+2022-09-27 17:20:09.331024
+2020-05-17 00:00:00
+August
+>>> 
+==================== RESTART: D:/py class python dates 1.py ====================
+2022-09-27 17:23:56.454239
+2020-05-17 00:00:00
+August
+Monday
+Mon
+Aug
+>>> 
+==================== RESTART: D:/py class python dates 1.py ====================
+2022-09-27 17:27:07.530905
+2020-05-17 00:00:00
+August
+Monday
+Mon
+Aug
+2022
+Tuesday
