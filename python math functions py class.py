e42c037f89af8e839f947bc79037c30204b46521
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-28 23:00:24 +0530

    Add files via upload

---
+++ python math functions py class.py
@@ -0,0 +1,52 @@
+#python math functions
+# Built in math functions
+
+#use min( ) and max( )
+
+#example:
+
+x=min(10,50,25)
+y=max(10,50,25)
+print(x)
+print(y)
+
+
+#abs ) function (it abs functions returns the absolute (positive) values of specified number
+
+x=abs(-7.25)
+
+print(x)
+
+
+# pow(x,y)function related to power i.e,x^y
+
+x= pow(5,6)
+print(x)
+
+
+#The Math Module
+#using import math
+
+#1. math.sqrt( )
+
+import math
+x=math.sqrt(54)
+
+print(x)
+
+#math.ceil( )function
+
+import math
+x=math.ceil(1.6)
+y=math.floor(1.8)
+
+print(x)#returns 2
+print(y)#returns 1
+
+
+#math.pi( )function
+import math
+x=math.pi
+print(x)
+
+
