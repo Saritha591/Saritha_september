b416f2c54a3e7a05647d74043b3c46fd8819bc77
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-28 10:46:20 +0530

    Update py class array 2.py

--- py class array 2.py
+++ py class array 2.py
@@ -9,34 +9,33 @@ print(type(x))
 
 
 #creating an array using iterator "for loop"
-'''import array
+import array
 x=array.array('i',[10,29,30,46,37])
 print(type(x))
 for i in x:
-    print(i,end=' ')'''
+    print(i,end=' ')
 
 #Append menthod
 #range( )
 # creating an array up to 'n'elements using append method
 
-'''import array
-x=array.array('i',[ ])
-n=int(input('Enter the range of an array....'))
-for i in range(n):
-    x.append(int(input('Enter an array elements...')))
-for i in x:
-   print(i,end=' ')'''
-
-# creating an array using len( )             
-        
-'''import array
+import array
 x=array.array('i',[ ])
 n=int(input('Enter the range of an array....'))
 for i in range(n):
     x.append(int(input('Enter an array elements...')))
 for i in x:
    print(i,end=' ')
-print('\n length of an array= ',len(x))'''
+
+# creating an array using len( )             
+import array
+x=array.array('i',[ ])
+n=int(input('Enter the range of an array....'))
+for i in range(n):
+    x.append(int(input('Enter an array elements...')))
+for i in x:
+   print(i,end=' ')
+print('\n length of an array= ',len(x))
 
 #Index value
 #create an array
