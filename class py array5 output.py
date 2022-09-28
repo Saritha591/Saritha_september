03c5f6a6019eb0ab2fe690e8b75cfb7f01081832
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-09-28 22:41:48 +0530

    Add files via upload

---
+++ class py array5 output.py
@@ -0,0 +1,24 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: D:/py class array5.py ========================
+enter the range of an array5
+enter an array element..4
+enter an array element..6
+enter an array element..5
+enter an array element..9
+enter an array element..2
+Traceback (most recent call last):
+  File "D:/py class array5.py", line 12, in <module>
+    for i in x:
+NameError: name 'x' is not defined
+>>> 
+======================== RESTART: D:/py class array5.py ========================
+enter the range of an array5
+enter an array element..3
+enter an array element..5
+enter an array element..4
+enter an array element..6
+enter an array element..8
+No.of even numbers= 3
+No.of odd numbers= 2
