10322644571b40600abbc639c1f4eac8a2594c2a
Saritha591 <113581523+Saritha591@users.noreply.github.com>
2022-10-07 20:10:29 +0530

    Add files via upload

---
+++ output task python tutorial 11.py
@@ -0,0 +1,170 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:45
+you got passing grade
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:34
+you got fail grade
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:56
+you got passing grade
+enter the integer value:saritha
+Traceback (most recent call last):
+  File "D:/task for python tutorial examples.py", line 11, in <module>
+    x=int(input("enter the integer value:"))
+ValueError: invalid literal for int() with base 10: 'saritha'
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:35
+you got passing grade
+enter the integer value:3.5
+Traceback (most recent call last):
+  File "D:/task for python tutorial examples.py", line 11, in <module>
+    x=int(input("enter the integer value:"))
+ValueError: invalid literal for int() with base 10: '3.5'
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:45
+you got passing grade
+enter the integer value:89
+not an integer value
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:56
+you got passing grade
+enter the integer value:45
+you get value
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+enter your marks in maths:45
+you got passing grade
+enter the integer value:67
+you get value
+enter score of indian team:....267
+we won the match
+celebrations done by ever citizen
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+cat 3
+window 6
+bell 4
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+cat 3
+window 6
+bell 4
+enter n value....24
+2 4 6 8 10 12 14 16 18 20 22 24 
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+cat 3
+window 6
+bell 4
+enter n value....16
+2 4 6 8 10 12 14 16 enter the n value...4
+1 3 
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+cat 3
+window 6
+bell 4
+enter n value....16
+2 4 6 8 10 12 14 16 enter the n value...16
+f=  1
+f=  2
+f=  6
+f=  24
+f=  120
+f=  720
+f=  5040
+f=  40320
+f=  362880
+f=  3628800
+f=  39916800
+f=  479001600
+f=  6227020800
+f=  87178291200
+f=  1307674368000
+f=  20922789888000
+
+=============== RESTART: D:/task for python tutorial examples.py ===============
+saritha is abc clg got: 56 marks
+zis greater
+cat 3
+window 6
+bell 4
+enter n value....5
+2 4 enter the n value...2
+f=  1
+f=  2
+10
+15
+20
+25
+Traceback (most recent call last):
+  File "D:/task for python tutorial examples.py", line 73, in <module>
+    pr
+NameError: name 'pr' is not defined
+
+=================== RESTART: D:/pratice 11 fibnacci series.py ==================
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+>>> 
+=================== RESTART: D:/pratice 11 fibnacci series.py ==================
+enter the value of n100
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+>>> 
+=================== RESTART: D:/pratice 11 fibnacci series.py ==================
+enter the value of n50
+1
+2
+3 5 8 13 21 34 55 
+>>> 
+=================== RESTART: D:/pratice 11 fibnacci series.py ==================
+enter the value of n50
+1
+1
+2 3 5 8 13 21 34 55 
---
+++ task for python tutorial examples.py
@@ -0,0 +1,89 @@
+#this program is whether student got passing marks or not
+
+'''marks=int(input("enter your marks in maths:"))
+if marks>=35:
+    print("you got passing grade")
+else:
+    print("you got fail grade")
+
+
+#ex:2
+x=int(input("enter the integer value:"))
+if x>=0:
+    print("you get value")
+else:
+    print("not an integer value")
+
+#ex:3 for if statements
+
+score=int(input("enter score of indian team:...."))
+if score>=250:
+    print("we won the match")
+print("celebrations done by ever citizen")'''
+
+#ex:4 nested if
+
+name="saritha"
+clg="abc"
+marks=56
+if clg == "abc":
+    if name == "saritha":
+        print("saritha is abc clg got:",marks,"marks")
+
+#ex:5 elif statement
+
+x=30
+y=40
+z=50
+if x>y and x>z:
+    print("x is greater")
+elif z>x and z>y:
+    print("zis greater")
+
+else:
+    print("y is greater")
+
+#for loop
+words=['cat','window','bell']
+for w in words:
+    print(w,len(w))
+
+
+#while loop
+#program to print even numbers
+
+i=2
+n=int(input('enter n value....'))
+while i <=n:
+    print(i,end=' ')
+    i+=2
+#program to print facorial of given number
+i=1
+f=1
+n=int(input('enter the n value...'))
+while i<=n:
+    f=f*i
+    i=i+1
+    print('f= ',f)
+
+#range( )
+#range function in for loop
+for i in range(10,30,5):
+    print(i)
+pr
+
+    
+    
+    
+
+
+    
+        
+          
+    
+
+      
+          
+            
+             
+        
